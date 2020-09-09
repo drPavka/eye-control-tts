9cf235b2b3e73a2ec80bf8308ed518fa64245b97
Pavlo Dubenko <d.pavka@gmail.com>
2020-09-09 11:42:04 +0300

    added logo made some tiny UI changes added dist folder for github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+const TEXT_TO_SPEECH_BASE = 'https://texttospeech.googleapis.com/v1/';
+
 export const environment = {
-  production: true
+    production: true,
+    //@todo  - rewrite. Only for test purposes it is shown on client
+    apiKeyObj: {
+        'X-Goog-Api-Key': 'AIzaSyBUrG7YyqBHH-TcgwACamVt3mlNU2u5dR4'
+    },
+    voicesList: TEXT_TO_SPEECH_BASE + 'voices',
+    synthesize: TEXT_TO_SPEECH_BASE + 'text:synthesize'
 };
